ed9f0c4f5d8695a63977232f237727147d32b9c1
Dante Vicenzo <dantevcz@gmail.com>
2023-05-31 17:35:47 -0300

    New transaction Dialog

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,56 @@
 import logo from '../../assets/logo.svg'
-import { Container, StyledHeader, NewTransactionButton } from './styles'
+import {
+  Container,
+  StyledHeader,
+  StyledButton,
+  Overlay,
+  Content,
+  Form,
+  RadioGroupRoot,
+  RadioGroupItem,
+  DialogClose,
+} from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
+import { ArrowCircleUp, ArrowCircleDown, X } from '@phosphor-icons/react'
 
 export function Header() {
   return (
     <Container>
       <StyledHeader>
         <img src={logo} alt="" />
-        <NewTransactionButton type="button">
-          Nova Transação
-        </NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <StyledButton type="button">Nova Transação</StyledButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Overlay />
+            <Content>
+              <strong>Nova Transação</strong>
+
+              <Form>
+                <input type="text" placeholder="Descrição" />
+                <input type="text" placeholder="Preço" />
+                <input type="text" placeholder="Categoria" />
+                <RadioGroupRoot>
+                  <RadioGroupItem value="income">
+                    <ArrowCircleUp size={24} />
+                    Entrada
+                  </RadioGroupItem>
+                  <RadioGroupItem value="outcome">
+                    <ArrowCircleDown size={24} />
+                    Saída
+                  </RadioGroupItem>
+                </RadioGroupRoot>
+                <StyledButton type="submit">Cadastrar</StyledButton>
+              </Form>
+
+              <DialogClose>
+                <X size={24} />
+              </DialogClose>
+            </Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </StyledHeader>
     </Container>
   )
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,4 +1,6 @@
 import styled from 'styled-components'
+import * as Dialog from '@radix-ui/react-dialog'
+import * as RadioGroup from '@radix-ui/react-radio-group'
 
 export const Container = styled.div`
   background-color: ${(props) => props.theme.grey1};
@@ -12,7 +14,7 @@ export const StyledHeader = styled.header`
   padding: 2.5rem 0 7.625rem;
 `
 
-export const NewTransactionButton = styled.button`
+export const StyledButton = styled.button`
   background-color: ${(props) => props.theme.green};
   color: ${(props) => props.theme.white};
 
@@ -29,4 +31,107 @@ export const NewTransactionButton = styled.button`
   &:hover {
     background-color: ${(props) => props.theme.greenLight};
   }
+`
+export const Overlay = styled(Dialog.Overlay)`
+  background: rgba(0 0 0 / 0.75);
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  inset: 0;
+`
+export const Content = styled(Dialog.Content)`
+  width: 33rem;
+  background-color: ${(props) => props.theme.grey2};
+  padding: 3rem 3rem 2.5rem;
+  border-radius: 6px;
+
+  strong {
+    color: ${(props) => props.theme.grey7};
+    font-size: 1.5rem;
+  }
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+export const Form = styled.form`
+  margin-top: 2rem;
+  display: flex;
+  flex-direction: column;
+
+  input + input {
+    margin-top: 1rem;
+  }
+
+  div[role='radiogroup'] {
+    margin-top: 1.5rem;
+  }
+
+  button[type='submit'] {
+    margin-top: 2.5rem;
+  }
+
+  input[type='text'] {
+    border-radius: 6px;
+    padding: 1rem;
+    background-color: ${(props) => props.theme.grey1};
+    border: none;
+    color: ${(props) => props.theme.grey6};
+  }
+`
+export const RadioGroupRoot = styled(RadioGroup.Root)`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  gap: 1rem;
+`
+export const RadioGroupItem = styled(RadioGroup.Item)`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  padding: 16px 24px;
+  border-radius: 6px;
+
+  background-color: ${(props) => props.theme.grey3};
+  color: ${(props) => props.theme.grey6};
+
+  font-size: 1rem;
+
+  border: none;
+
+  cursor: pointer;
+
+  svg {
+    color: ${({ value, theme }) =>
+      value === 'income' ? theme.greenLight : theme.red};
+  }
+
+  &:hover {
+    background-color: ${(props) => props.theme.grey4};
+  }
+
+  &[data-state='checked'] {
+    background-color: ${({ value, theme }) =>
+      value === 'income' ? theme.greenDark : theme.redDark};
+
+    color: ${(props) => props.theme.white};
+
+    svg {
+      color: ${(props) => props.theme.white};
+    }
+  }
+`
+
+export const DialogClose = styled(Dialog.Close)`
+  position: absolute;
+  top: 24px;
+  right: 24px;
+  line-height: 0;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+
+  color: ${(props) => props.theme.grey5};
 `
